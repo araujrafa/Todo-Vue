20651870c5f1ca6a739f19d9b0e448dd4f62fa8d
Rafael Araujo <araujrafa@gmail.com>
2018-03-16 22:45:19 -0300

    Adicionando arquivos do todo

---
+++ src/components/Todo.vue
@@ -0,0 +1,59 @@
+<template>
+  <div class="ui centered card">
+    <div class="content" v-show="!isEdition">
+      <div class="header">
+        {{ todo.title }}
+      </div>
+      <div class="meta">
+        {{ todo.project }}
+      </div>
+      <div class="extra content">
+        <span class="right floated edit icon" v-on:click="showForm">
+          <i class="edit icon"></i>
+        </span>
+      </div>
+    </div>
+    <div class="content" v-show="isEdition">
+      <div class='ui form'>
+        <div class="field">
+          <label>Titulo</label>
+          <input type="text" v-model="todo.title">
+        </div>
+        <div class="field">
+          <label>Projeto</label>
+          <input type="text" v-model="todo.project">
+        </div>
+        <div class='ui two button attached buttons'>
+          <button class='ui basic blue button' v-on:click="hideForm">
+            Close X
+          </button>
+        </div>
+      </div>
+    </div>
+    <div class='ui bottom attached green basic button' v-show="!isEditing &&todo.done" disabled>
+        Completada
+    </div>
+    <div class='ui bottom attached red basic button' v-show="!isEditing && !todo.done">
+        Pendente
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ['todo'],
+  data () {
+    return {
+      isEdition: false
+    }
+  },
+  methods: {
+    showForm () {
+      this.isEdition = true
+    },
+    hideForm () {
+      this.isEdition = false
+    }
+  }
+}
+</script>
---
+++ src/components/TodoList.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <p>Tarefas completadas: {{todos.filter(todo => {return todo.done === true}).length}}</p>
+    <p>Tarefas pendetes: {{todos.filter(todo => {return todo.done === false}).length}}</p>
+    <todo
+      v-for="(todo, index) in todos"
+      v-bind:todo="todo"
+      :key="index">
+    </todo>
+  </div>
+</template>
+
+<script type="text/javascript">
+
+import Todo from './Todo'
+
+export default {
+  props: ['todos'],
+  components: {
+    Todo
+  }
+}
+
+</script>
+<style>
+
+</style>
